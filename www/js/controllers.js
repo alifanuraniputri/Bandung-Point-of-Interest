c045ea890b470de371a33bc74f2d38b3c551fcdc
alifanuraniputri <alifanuraniputri@gmail.com>
2015-06-16 18:23:19 +0700

    before adding location

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -220,6 +220,37 @@ angular.module('starter.controllers', ['uiGmapgoogle-maps'])
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
 
+.controller("loginFBCtrl", function($scope, $rootScope, $firebase, $firebaseSimpleLogin) {
+  // Get a reference to the Firebase
+  // TODO: Replace "ionic-demo" below with the name of your own Firebase
+  var firebaseRef = new Firebase("https://bandung-poi.firebaseio.com/");
+  // Create a Firebase Simple Login object
+  $scope.auth = $firebaseSimpleLogin(firebaseRef);
+  // Initially set no user to be logged in
+  $scope.user = null;
+  // Logs a user in with inputted provider
+  $scope.login = function(provider) {
+    $scope.auth.$login(provider);
+  };
+  // Logs a user out
+  $scope.logout = function() {
+    $scope.auth.$logout();
+  };
+  // Upon successful login, set the user object
+  $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
+    $scope.user = user;
+  });
+  // Upon successful logout, reset the user object
+  $rootScope.$on("$firebaseSimpleLogin:logout", function(event) {
+    $scope.user = null;
+  });
+  // Log any login-related errors to the console
+  $rootScope.$on("$firebaseSimpleLogin:error", function(event, error) {
+    console.log("Error logging user in: ", error);
+  });
+  
+})
+
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
 
 
